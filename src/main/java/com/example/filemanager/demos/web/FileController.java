ba541104849c92940d14a1b7dcacf08029e89374
liuxincheng <sciencestar9346@163.com>
2023-12-07 14:23:55 +0800

    文件管理服务器初始化

---
+++ src/main/java/com/example/filemanager/demos/web/FileController.java
@@ -0,0 +1,45 @@
+package com.example.filemanager.demos.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@Controller
+public class FileController {
+
+    @GetMapping("/download/{targetFile}")
+    public HttpServletResponse download(@PathVariable("targetFile") String targetFile,
+                                        HttpServletResponse response) {
+
+        try {
+            String path = "D:\\temp1\\" + targetFile;
+            // path是指欲下载的文件的路径。
+            File file = new File(path);
+            // 取得文件名。
+            String filename = file.getName();
+            // 取得文件的后缀名。
+            String ext = filename.substring(filename.lastIndexOf(".") + 1).toUpperCase();
+            // 以流的形式下载文件。
+            InputStream fis = new BufferedInputStream(new FileInputStream(path));
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            fis.close();
+            // 清空response
+            response.reset();
+            // 设置response的Header
+            response.addHeader("Content-Disposition", "attachment;filename=" + new String(filename.getBytes()));
+            response.addHeader("Content-Length", "" + file.length());
+            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+            response.setContentType("application/octet-stream");
+            toClient.write(buffer);
+            toClient.flush();
+            toClient.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return response;
+    }
+}
